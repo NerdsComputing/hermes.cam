94a1706d44921284ea20b1bf402a8649705dd8c3
radu <39530866+RaduBuciuceanu@users.noreply.github.com>
2021-06-16 23:23:57 +0300

    [chaos] Configure client side prediction (#5)
    * [chaos] Reference tensorflow packages
    
    * [chaos] Detect objects
    
    * [chaos] Remove app component
    
    * [chaos] Change index component
    
    * [chaos] Create app components
    
    * [chaos] Create app context
    
    * [chaos] Create app repository
    
    * [chaos] Create use coroutine
    
    * [chaos] Create use prediction

---
+++ src/app/context.js
@@ -0,0 +1,16 @@
+import React, {useRef, useState} from "react";
+
+export const Context = React.createContext({})
+
+export const useContext = () => React.useContext(Context)
+
+export const useValue = () => {
+  const camera = useRef()
+  const [objects, setObjects] = useState([])
+
+  return {
+    camera,
+    objects,
+    setObjects
+  }
+}
