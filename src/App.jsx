9471d6b70ddfe4686ebf08f9b2bf21d949cd3b9c
Radu B <radu.buciuceanu@gmail.com>
2021-06-16 20:27:51 +0300

    [chaos] Detect objects

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,33 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 
-export const App = () => <h1>Hermes cam</h1>
+import '@tensorflow/tfjs-backend-cpu';
+import '@tensorflow/tfjs-backend-webgl';
+import {load} from '@tensorflow-models/coco-ssd'
+import Webcam from 'react-webcam'
+import {delay, mergeMap, repeat, tap} from "rxjs/operators";
+import {from} from "rxjs";
+
+export const App = () => {
+  const ref = useRef()
+
+  const [objects, setObjects] = useState([])
+
+  useEffect(() => {
+    const subscription = from(load({base: "mobilenet_v2"}))
+      .pipe(mergeMap(model => from(model.detect(ref.current.video))))
+      .pipe(tap(setObjects))
+      .pipe(delay(300))
+      .pipe(repeat())
+      .subscribe()
+
+
+    return () => subscription.unsubscribe()
+  }, [])
+
+  return <>
+    <Webcam ref={ref}/>
+    {
+      objects.map(item => <li>{item.class}</li>)
+    }
+  </>
+}
