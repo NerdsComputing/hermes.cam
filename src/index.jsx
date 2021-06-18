05de44f6f2d5fb9feb81324870490be02e7905a8
Mihai Radulescu <mihai.radulescu@nerdscomputing.com>
2021-06-18 17:12:28 +0300

    [google-tracking] Integrate google tracking (#6)
    * [google-tracking] add google tracking code for marketing purposes
    
    * [google-tracking] fix version in package.json

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,10 @@ import ReactDOM from 'react-dom';
 import {App} from './app';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-199841319-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <React.StrictMode>
