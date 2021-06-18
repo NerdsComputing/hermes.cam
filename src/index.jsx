f00b89b87008f1c62e66173a981b344a56168039
Radu B <radu.buciuceanu@gmail.com>
2021-06-18 21:39:35 +0300

    [chaos] Change index component

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import {App} from './app';
+import {Startup} from "./startup";
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import ReactGA from 'react-ga';
-
-ReactGA.initialize('UA-199841319-1');
-ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Startup />
   </React.StrictMode>,
   document.getElementById('root')
 );
