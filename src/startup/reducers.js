1bbbdda63b2ed1ced379cacd3f26b574c0beb43b
Radu B <radu.buciuceanu@gmail.com>
2021-08-02 11:55:19 +0300

    [chaos] Fix eslint issues

--- src/startup/reducers.js
+++ src/startup/reducers.js
@@ -2,10 +2,10 @@ import { combineReducers } from '@reduxjs/toolkit'
 
 import { loading } from 'loading/slices/loading'
 import { settings } from 'settings/slices/settings'
-import {register} from 'camera/slices/register'
+import { register } from 'camera/slices/register'
 
 export const reducers = combineReducers({
   settings: settings.reducer,
   loading: loading.reducer,
-  register: register.reducer
+  register: register.reducer,
 })
