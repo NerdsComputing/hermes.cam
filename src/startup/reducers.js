5bd3cf620509f0169116f261dba07bbd86dfe35d
Radu B <radu.buciuceanu@gmail.com>
2021-08-02 17:46:35 +0300

    [HER-136] Remove loading reducer

--- src/startup/reducers.js
+++ src/startup/reducers.js
@@ -1,11 +1,8 @@
+import { loading } from 'loading/slices/loading'
+import { register } from 'camera/slices/register'
 import { combineReducers } from '@reduxjs/toolkit'
 
-import { loading } from 'loading/slices/loading'
-import { settings } from 'settings/slices/settings'
-import { register } from 'camera/slices/register'
-
 export const reducers = combineReducers({
-  settings: settings.reducer,
   loading: loading.reducer,
   register: register.reducer,
 })
