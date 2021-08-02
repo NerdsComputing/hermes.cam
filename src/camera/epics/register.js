a9d4d4ecb9390b1304679d52659dfbe199de84b1
Radu B <radu.buciuceanu@gmail.com>
2021-08-02 12:48:27 +0300

    [chaos] Create register epic

--- src/camera/epics/register.js
+++ src/camera/epics/register.js
@@ -1,10 +1,24 @@
-import { of } from 'rxjs'
+import { from, of } from 'rxjs'
 import { ofType } from 'redux-observable'
-import { mergeMap } from 'rxjs/operators'
-import { actions } from 'camera/slices/register'
+import * as loadingSlice from 'loading/slices/loading'
+import * as registerSlice from 'camera/slices/register'
+import { catchError, delay, mergeMap } from 'rxjs/operators'
 
 export const register = action$ => action$
-  .pipe(ofType(actions.attempt.type))
+  .pipe(ofType(registerSlice.actions.attempt.type))
   .pipe(mergeMap(perform))
 
-const perform = action => of(actions.success({}))
+const perform = () => of({})
+  .pipe(delay(3000))
+  .pipe(mergeMap(successActions))
+  .pipe(catchError(failureActions))
+
+const successActions = visitorId => from([
+  registerSlice.actions.success(visitorId),
+  loadingSlice.actions.hide(registerSlice.actions.attempt.type),
+])
+
+const failureActions = error => from([
+  registerSlice.actions.failure(error),
+  loadingSlice.actions.hide(registerSlice.actions.attempt.type),
+])
