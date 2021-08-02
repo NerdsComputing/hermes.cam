0a68c80bb466f223e66a9200a9b8d3258846cccb
Radu B <radu.buciuceanu@gmail.com>
2021-08-02 16:21:34 +0300

    [HER-132] Change behaviour component

--- src/camera/behaviour.jsx
+++ src/camera/behaviour.jsx
@@ -1,8 +1,12 @@
 import { usePrediction } from 'camera/hooks/use-prediction'
 import { useRegistering } from 'camera/hooks/use-registering'
+import { useModelLoading } from 'camera/hooks/use-model-loading'
+import { useCameraLoading } from 'camera/hooks/use-camera-loading'
 
 export const Behaviour = () => {
   useRegistering()
+  useCameraLoading()
+  useModelLoading()
   usePrediction()
 
   return ''
