c4a0fa7118e43ff202727da166174d6ed97b313c
Radu B <radu.buciuceanu@gmail.com>
2021-08-02 23:12:34 +0300

    [HER-136] Change camera behaviour

--- src/camera/behaviour.jsx
+++ src/camera/behaviour.jsx
@@ -2,8 +2,10 @@ import { usePrediction } from 'camera/hooks/use-prediction'
 import { useRegistering } from 'camera/hooks/use-registering'
 import { useModelLoading } from 'camera/hooks/use-model-loading'
 import { useCameraLoading } from 'camera/hooks/use-camera-loading'
+import { useCameraFetching } from 'camera/hooks/use-camera-fetching'
 
 export const Behaviour = () => {
+  useCameraFetching()
   useRegistering()
   useCameraLoading()
   useModelLoading()
