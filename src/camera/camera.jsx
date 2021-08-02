b7882cf1237df6842006d627befff89c6e0c4569
Radu B <radu.buciuceanu@gmail.com>
2021-08-02 11:54:36 +0300

    [chaos] Change camera component

--- src/camera/camera.jsx
+++ src/camera/camera.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import Webcam from 'react-webcam'
 
 import { useContext } from 'camera/context'
+import { useShouldHide } from 'camera/hooks/use-should-hide'
 
 export const Camera = () => {
   const { camera } = useContext()
+
+  if (useShouldHide()) {
+    return <div />
+  }
 
   return <Webcam ref={camera} videoConstraints={{ facingMode: 'environment' }} />
 }
