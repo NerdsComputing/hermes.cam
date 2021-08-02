e47d793ff575e342df65c0bab849f5c0fd51351b
Radu B <radu.buciuceanu@gmail.com>
2021-08-02 20:18:28 +0300

    [HER-136] Register new reducers

--- src/startup/reducers.js
+++ src/startup/reducers.js
@@ -1,8 +1,8 @@
+import { camera } from 'camera/slices'
 import { loading } from 'loading/slices/loading'
-import { register } from 'camera/slices/register'
 import { combineReducers } from '@reduxjs/toolkit'
 
 export const reducers = combineReducers({
   loading: loading.reducer,
-  register: register.reducer,
+  camera,
 })
