9bce107bec0a7d97a330ce4896b8506758ff5973
Radu B <radu.buciuceanu@gmail.com>
2021-08-02 17:46:20 +0300

    [HER-136] Remove loading epic

--- src/startup/epics.js
+++ src/startup/epics.js
@@ -1,5 +1,4 @@
-import { fetch } from 'settings/epics/fetch'
 import { combineEpics } from 'redux-observable'
 import { register } from 'camera/epics/register'
 
-export const epics = combineEpics(fetch, register)
+export const epics = combineEpics(register)
