073d49f6951bfbd96e8b7ef89134e540ce2b4878
Radu B <radu.buciuceanu@gmail.com>
2021-08-02 11:54:54 +0300

    [chaos] Register new slice

--- src/startup/reducers.js
+++ src/startup/reducers.js
@@ -2,8 +2,10 @@ import { combineReducers } from '@reduxjs/toolkit'
 
 import { loading } from 'loading/slices/loading'
 import { settings } from 'settings/slices/settings'
+import {register} from 'camera/slices/register'
 
 export const reducers = combineReducers({
   settings: settings.reducer,
   loading: loading.reducer,
+  register: register.reducer
 })
