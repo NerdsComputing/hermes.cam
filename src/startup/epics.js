81836fa21271fb38c65e07746b570a6bc71e8f53
Radu B <radu.buciuceanu@gmail.com>
2021-08-02 12:49:39 +0300

    [chaos] Register the new epic

--- src/startup/epics.js
+++ src/startup/epics.js
@@ -1,5 +1,5 @@
-import { combineEpics } from 'redux-observable'
-
 import { fetch } from 'settings/epics/fetch'
+import { combineEpics } from 'redux-observable'
+import { register } from 'camera/epics/register'
 
-export const epics = combineEpics(fetch)
+export const epics = combineEpics(fetch, register)
