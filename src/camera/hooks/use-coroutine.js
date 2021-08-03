6c18b547ab4d606a1a83e4a033f461c184fe14cb
Radu B <radu.buciuceanu@gmail.com>
2021-08-03 17:16:19 +0300

    [HER-141] Change camera hooks

--- src/camera/hooks/use-coroutine.js
+++ src/camera/hooks/use-coroutine.js
@@ -1,14 +1,23 @@
 import { from, of } from 'rxjs'
-import { useCallback } from 'react'
+import { useDispatch } from 'react-redux'
 import { useContext } from 'camera/context'
+import { useCallback, useMemo } from 'react'
+import { actions } from 'camera/slices/detections'
 import { delay, mergeMap, repeat, tap } from 'rxjs/operators'
 
+const useParams = () => {
+  const { camera } = useContext()
+  const dispatch = useDispatch()
+
+  return useMemo(() => ({ camera, dispatch }), [camera, dispatch])
+}
+
 export const useCoroutine = () => {
-  const { camera, setObjects } = useContext()
+  const params = useParams()
 
   return useCallback(model => of([])
-    .pipe(mergeMap(() => from(model.detect(camera.video))))
-    .pipe(tap(setObjects))
+    .pipe(mergeMap(() => from(model.detect(params.camera.video))))
+    .pipe(tap(detection => params.dispatch(actions.buffer(detection))))
     .pipe(delay(1000))
-    .pipe(repeat()), [camera, setObjects])
+    .pipe(repeat()), [params])
 }
