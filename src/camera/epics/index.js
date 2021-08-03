f4e4914431511c83b5c11cb94b180a9f82decbf4
Radu B <radu.buciuceanu@gmail.com>
2021-08-03 13:31:57 +0300

    [HER-138] Change camera epics

--- src/camera/epics/index.js
+++ src/camera/epics/index.js
@@ -1,5 +1,6 @@
 import { fetch } from 'camera/epics/fetch'
 import { combineEpics } from 'redux-observable'
 import { register } from 'camera/epics/register'
+import { fetchFingerprint } from 'camera/epics/fetch-fingerprint'
 
-export const camera = combineEpics(fetch, register)
+export const camera = combineEpics(fetch, register, fetchFingerprint)
--- src/camera/epics/register.js
+++ src/camera/epics/register.js
@@ -1,15 +1,15 @@
-import { from, of } from 'rxjs'
+import { from } from 'rxjs'
 import { ofType } from 'redux-observable'
+import { catchError, mergeMap } from 'rxjs/operators'
 import * as loadingSlice from 'loading/slices/loading'
 import * as registerSlice from 'camera/slices/register'
-import { catchError, delay, mergeMap } from 'rxjs/operators'
+import * as repository from 'camera/repositories/camera'
 
 export const register = action$ => action$
   .pipe(ofType(registerSlice.actions.attempt.type))
   .pipe(mergeMap(perform))
 
-const perform = () => of({})
-  .pipe(delay(3000))
+const perform = action => repository.register(action.payload)
   .pipe(mergeMap(successActions))
   .pipe(catchError(failureActions))
 
