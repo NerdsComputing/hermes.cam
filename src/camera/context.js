9efd3febdc7f82a668e9ea394a3df20a28071d12
Radu B <radu.buciuceanu@gmail.com>
2021-08-03 17:15:34 +0300

    [HER-141] Change camera context

--- src/camera/context.js
+++ src/camera/context.js
@@ -7,8 +7,7 @@ export const useContext = () => React.useContext(Context)
 export const useValue = () => {
   const [camera, setCamera] = useState(undefined)
   const [cameraReady, setCameraReady] = useState(false)
-  const [objects, setObjects] = useState([])
   const [model, setModel] = useState(undefined)
 
-  return { camera, setCamera, objects, setObjects, model, setModel, cameraReady, setCameraReady }
+  return { camera, setCamera, model, setModel, cameraReady, setCameraReady }
 }
